34000eac2d0cc229020c3137f987f0ccec03ae80
Lidiya_cutie <lidushehca188@gmail.com>
2023-03-09 01:39:15 +0300

    add readme.md, find_outliers.py, images and data

---
+++ out_liers_lib/find_outliers.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+
+def outliers_iqr_mod(data, feature, left=1.5, right=1.5, log_scale=False):
+    """
+    Находит выбросы в данных, используя метод межквартильного размаха.
+    Классический метод модифицирован путем добавления:
+    * возможности логарифмирования распределения
+    * ручного управления количеством межквартильных размахов в обе стороны распределения
+    Args:
+        data (pandas.DataFrame): набор данных
+        feature (str): имя признака, на основе которого происходит поиск выбросов
+        left (float, optional): количество межквартильных размахов в левую сторону распреденения. По умолчанию 1.5.
+        right (float, optional): количество межквартильных размахов в правую сторону распреденения. По умолчанию 1.5.
+        log_scale (bool, optional): режим логарифмирования. По умолчанию False - логарифмирование не применяется
+        
+    Returns:
+        pandas.DataFrame: наблюдения, попавшие в разряд выбросов
+        pandas.DataFrame: очищенные данные, из которых исключены выбросы
+    """
+    if log_scale:
+        x = np.log(data[feature]+1)
+    else:
+        x = data[feature]
+    quartile_1, quartile_3 = x.quantile(0.25), x.quantile(0.75),
+    iqr = quartile_3 - quartile_1
+    lower_bound = quartile_1 - (iqr * left)
+    upper_bound = quartile_3 + (iqr * right)
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x > lower_bound) & (x < upper_bound)]
+    return outliers, cleaned
+
+def outliers_z_score_mod(data, feature, left=3, right=3, log_scale=False):
+    """
+    Находит выбросы в данных, используя метод z-отклонений.
+    Классический метод модифицирован путем добавления:
+    * возможности логарифмирования распределения
+    * ручного управления количеством межквартильных размахов в обе стороны распределения
+    Args:
+        data (pandas.DataFrame): набор данных
+        feature (str): имя признака, на основе которого происходит поиск выбросов
+        left (float, optional): количество межквартильных размахов в левую сторону распреденения. По умолчанию 3.
+        right (float, optional): количество межквартильных размахов в правую сторону распреденения. По умолчанию 3.
+        log_scale (bool, optional): режим логарифмирования. По умолчанию False - логарифмирование не применяется
+        
+    Returns:
+        pandas.DataFrame: наблюдения, попавшие в разряд выбросов
+        pandas.DataFrame: очищенные данные, из которых исключены выбросы
+    """
+    if log_scale:
+        x = np.log(data[feature]+1)
+    else:
+        x = data[feature]
+    mu = x.mean()
+    sigma = x.std()
+    lower_bound = mu - left * sigma 
+    upper_bound = mu + right * sigma
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x > lower_bound) & (x < upper_bound)]
+    return outliers, cleaned
